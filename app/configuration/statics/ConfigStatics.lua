32113b6e6944f287ada7763c6b2628936db576f3
Athos Benther <athum@live.com>
2023-10-25 15:24:50 -0300

    Renames /statis/statics.lua to ConfigStatics to better reflect its content

---
+++ app/configuration/statics/ConfigStatics.lua
@@ -0,0 +1,65 @@
+ConfigStatics = {
+    forbiddenWeapons = {
+        -- "Silverhand",
+        -- "Zhuo",
+        -- "Warden",
+        -- "Palica",
+        -- "Authority",
+        -- "Borg4a",
+        "RocketLauncher",
+        -- "Blade",
+        "Vehicle_Power_Weapon",
+        "Vehicle_Power_Weapon_OutlawHeist",
+        "Machete_Kukri",
+        "Machete_Borg",
+        "One_Hand_Blade"
+    },
+    forbiddenVariationTerms = {
+        "Retrofix",
+        "Left_Hand",
+        "Scene",
+        "Hologram",
+        "2020",
+        "Unbreakable"
+    },
+    range = {
+        'MeleeWeapon',
+        'RangedWeapon'
+    },
+    class = {
+        'BladeWeapon',
+        'BluntWeapon',
+        'HeavyWeapon',
+        'PowerWeapon',
+        'SmartWeapon',
+        'TechWeapon',
+        'ThrowableWeapon',
+        --'OneHandedRangedWeapon',
+        'Special'
+    },
+    kind = {
+        'Grenade Launcher',
+        'Handgun',
+        'HMG',
+        'LMG',
+        'Revolver',
+        'Rifle Assault',
+        'Rifle Precision',
+        'Rifle Sniper',
+        'ShotgunWeapon',
+        'Shotgun Dual',
+        'SMG',
+
+        'Baton',
+        'BladeWeapon',
+        'One Hand Blade',
+        'One Hand Club',
+        'Katana',
+        'Knife',
+        'Two Hand Club',
+        'Two Hand Hammer',
+        'Knuckles'
+    }
+}
+
+return ConfigStatics
